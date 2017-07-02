0e92917bdf7a9f4d5045f4f3485772b5c09728e7
Mahendra Dora <pinku4132@gmail.com>
2017-07-02 22:54:06 +0530

    FrontEnd,Angular2 and Routing implemented

---
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import {Component,OnInit} from "@angular/core";
+
+@Component({
+selector:'app-navbar',
+templateUrl:`./navbar.component.html`,
+styleUrls:[]
+})
+
+export class NavbarComponent implements OnInit{
+
+    constructor(){
+
+    }
+    
+    ngOnInit(){
+
+    }
+
+}
